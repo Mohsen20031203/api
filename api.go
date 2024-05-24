4867645a063a61e403f507ccd967757cb26bf066
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-05-24 15:17:50 +0330

    start
    get
    post
    put

---
+++ api.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type PostData struct {
+	Message string `json:"message"`
+	Name    string `json:"Name"`
+}
+
+func main() {
+	e := echo.New()
+
+	data := &PostData{
+		Message: "Hello",
+		Name:    "World",
+	}
+
+	e.GET("mohsen/get", func(c echo.Context) error {
+		return c.String(http.StatusOK, data.Message)
+	})
+
+	e.POST("mohsen/post", func(c echo.Context) error {
+
+		var postData PostData
+		if err := c.Bind(&postData); err != nil {
+			return err
+		}
+
+		data = &postData
+
+		return c.JSON(http.StatusOK, data)
+
+	})
+
+	e.PUT("moshen/put", func(c echo.Context) error {
+		var putData PostData
+		if err := c.Bind(&putData); err != nil {
+			return err
+		}
+		data.Name = putData.Name
+
+		return c.JSON(http.StatusOK, data)
+	})
+
+	e.Start(":8080")
+}
